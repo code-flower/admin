6d2987cab3bc6ebb3a9714f88a84408be313f23c
Jake Mensch <jake.mensch@gmail.com>
2019-03-22 09:48:21 -0700

    status report format

--- bin/status.sh
+++ bin/status.sh
@@ -5,6 +5,7 @@ require('module-alias/register');
 /////////////////// IMPORTS //////////////////////
 
 const Promise = require('bluebird');
+const moment = require('moment');
 const rp = require('request-promise');
 const config = require('@config');
 const { listIPsForTag } = require('@lib/DO').droplets;
@@ -39,15 +40,38 @@ function getLogs(IPs) {
     return exec(`ssh root@${IP} 'cat ${REMOTE_LOG_FILE}'`)
       .catch(() => `No log file for ${IP}.`)
   })
+  // .then(logs => {
+  //   IPs.forEach((IP, idx) => {
+  //     console.log(`------------- ${IP} ---------------`);
+  //     if (logs[idx])
+  //       console.log(logs[idx]);
+  //     else
+  //       console.log('No log content.\n');
+  //   })
+  //   return null;
+  // });
+  .then(logs => logs.map((log, idx) => {
+    return log
+      .split('\n')
+      .filter(entry => !!entry)
+  }))
+  .then(logs => logs.reduce((p, c) => p.concat(c), []))
+  .then(entries => entries.sort((a, b) => {
+    let aDate = a.match(/(\d{4}.*?\d{2}):\s/)[1];
+    let bDate = b.match(/(\d{4}.*?\d{2}):\s/)[1];
+    return moment(bDate).isBefore(moment(aDate)) ? 1 : -1;
+  }))
+  .then(entries => entries.reduce((p, c) => {
+    if (c.indexOf('SUCCESS') !== -1)
+      p.success.push(c);
+    else
+      p.error.push(c);
+    return p;
+  }, { success: [], error: [] }))
   .then(logs => {
-    IPs.forEach((IP, idx) => {
-      console.log(`------------- ${IP} ---------------`);
-      if (logs[idx])
-        console.log(logs[idx]);
-      else
-        console.log('No log content.\n');
-    })
-    return null;
+    logs.success.forEach(log => console.log(log));
+    console.log('');
+    logs.error.forEach(log => console.log(log));
   });
 }
 
@@ -66,6 +90,3 @@ listIPsForTag(CLOC_SERVER_TAG)
       .then(() => getLogs(IPs))
       .then(() => clearLogs(IPs));
   });
-
-
-
